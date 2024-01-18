692adf8da08941dff1de7bede471bcd5c5a847dc
Alex Servirog <servalex1@gmail.com>
2024-01-18 20:44:00 +0200

    recreated migrations

---
+++ DebtBot.DB/Migrations/20240118181124_Initial.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DebtBot.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bills",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CurrencyCode = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bills", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    CurrencyCode = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    FullName = table.Column<string>(type: "text", nullable: false),
+                    Character = table.Column<char>(type: "character(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.CurrencyCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Parameter = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DisplayName = table.Column<string>(type: "text", nullable: false),
+                    TelegramId = table.Column<long>(type: "bigint", nullable: true),
+                    Phone = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BillLines",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BillId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ItemDescription = table.Column<string>(type: "text", nullable: false),
+                    Subtotal = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BillLines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BillLines_Bills_BillId",
+                        column: x => x.BillId,
+                        principalTable: "Bills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BillPayments",
+                columns: table => new
+                {
+                    BillId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(10,4)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BillPayments", x => new { x.BillId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_BillPayments_Bills_BillId",
+                        column: x => x.BillId,
+                        principalTable: "Bills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BillPayments_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Debts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreditorUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DebtorUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(10,4)", nullable: false),
+                    CurrencyCode = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Debts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Debts_Users_CreditorUserId",
+                        column: x => x.CreditorUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Debts_Users_DebtorUserId",
+                        column: x => x.DebtorUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ledgers",
+                columns: table => new
+                {
+                    CreditorUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DebtorUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Currency = table.Column<string>(type: "text", nullable: false),
+                    BillId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(10,4)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ledgers", x => new { x.CreditorUserId, x.DebtorUserId, x.Currency });
+                    table.ForeignKey(
+                        name: "FK_Ledgers_Bills_BillId",
+                        column: x => x.BillId,
+                        principalTable: "Bills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ledgers_Users_CreditorUserId",
+                        column: x => x.CreditorUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ledgers_Users_DebtorUserId",
+                        column: x => x.DebtorUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserContactsLinks",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContactUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DisplayName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserContactsLinks", x => new { x.UserId, x.ContactUserId });
+                    table.ForeignKey(
+                        name: "FK_UserContactsLinks_Users_ContactUserId",
+                        column: x => x.ContactUserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserContactsLinks_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BillLineParticipants",
+                columns: table => new
+                {
+                    BillLineId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Part = table.Column<decimal>(type: "numeric(10,4)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BillLineParticipants", x => x.BillLineId);
+                    table.ForeignKey(
+                        name: "FK_BillLineParticipants_BillLines_BillLineId",
+                        column: x => x.BillLineId,
+                        principalTable: "BillLines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BillLineParticipants_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BillLineParticipants_UserId",
+                table: "BillLineParticipants",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BillLines_BillId",
+                table: "BillLines",
+                column: "BillId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BillPayments_UserId",
+                table: "BillPayments",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Debts_CreditorUserId",
+                table: "Debts",
+                column: "CreditorUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Debts_DebtorUserId",
+                table: "Debts",
+                column: "DebtorUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ledgers_BillId",
+                table: "Ledgers",
+                column: "BillId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ledgers_DebtorUserId",
+                table: "Ledgers",
+                column: "DebtorUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserContactsLinks_ContactUserId",
+                table: "UserContactsLinks",
+                column: "ContactUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BillLineParticipants");
+
+            migrationBuilder.DropTable(
+                name: "BillPayments");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+
+            migrationBuilder.DropTable(
+                name: "Debts");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Ledgers");
+
+            migrationBuilder.DropTable(
+                name: "UserContactsLinks");
+
+            migrationBuilder.DropTable(
+                name: "BillLines");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Bills");
+        }
+    }
+}
