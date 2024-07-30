de87fe11f48dea674c507bf2d8092a887932985e
Kirill Chekanov <checkatillo@gmail.com>
2024-07-30 17:29:50 +0200

    Exchange preview

--- DebtBot/Consumers/Notification/ExchangeCreatedConsumer.cs
+++ DebtBot/Consumers/Notification/ExchangeCreatedConsumer.cs
@@ -21,13 +21,33 @@ public class ExchangeCreatedConsumer : IConsumer<ExchangeCreated>
     {
         var exchangeData = context.Message;
 
+        var forwardBill = await _billService.GetAsync(exchangeData.ForwardBillId, context.CancellationToken);
+        var backwardBill = await _billService.GetAsync(exchangeData.BackwardBillId, context.CancellationToken);
+
+        var forwardPayment = forwardBill?.Payments.FirstOrDefault();
+        var backwardPayment = backwardBill?.Payments.FirstOrDefault();
+
+        if (forwardPayment is null || backwardPayment is null)
+        {
+            await _publishEndpoint.Publish(new TelegramMessageRequested(
+                exchangeData.ChatId,
+                "Error creating exchange"),
+                context.CancellationToken);
+
+            return;
+        }
+
+        var text = $"Exchange created:\n"
+            + $"{forwardPayment.User} will owe {backwardPayment.User} extra {backwardPayment.Amount:0.##} {backwardBill!.PaymentCurrencyCode}\n"
+            + $"{backwardPayment.User} will owe {forwardPayment.User} extra {forwardPayment.Amount:0.##} {forwardBill!.PaymentCurrencyCode}";
+
         var markup = new List<InlineButtonRecord>();
 
         markup.Add(new("Finalize", FinalizeExchangeCommand.FormatCallbackData(exchangeData.ForwardBillId, exchangeData.BackwardBillId)));
 
         await _publishEndpoint.Publish(new TelegramMessageRequested(
             exchangeData.ChatId,
-            "<b>Exchange created</b>",
+            text,
             InlineKeyboard: [markup]),
             context.CancellationToken);
 
